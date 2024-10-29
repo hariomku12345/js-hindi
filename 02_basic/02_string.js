e9a53874b5548397ed0b78f6184971e055ce373d
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-29 20:26:54 +0000

    string method

---
+++ 02_basic/02_string.js
@@ -0,0 +1,20 @@
+// const name = "hariom"
+// const repoCount = 100
+
+// console.log(name + repoCount + "value")
+//above line is outdated tady we are prefare to not use
+
+// console.log(`hello my name is ${name} and my repoCount is ${repoCount}`)
+// above code is more readable
+
+// 2nd method is initilised string
+
+const gameName = new String('hariom')
+// console.log(gameName)
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('o'))
