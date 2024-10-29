7ce8b993eab8d055c2527474f21dca98de3e35e6
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-29 19:05:33 +0000

    comparesion in javascript

---
+++ 01_basic/05_comparesion.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// we avoid above type of comparesion
+
+//  == strictly check mahi karta h
+// === strictly check karta h datatype bhi dekhta h
+console.log("2" == 2)
+console.log("2" === 2)
