fa5d2cd0080c8fcfa10f479f2f3b72869aa3ad2b
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-29 09:08:19 +0000

    conversion of datatype

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,20 @@
+let score =25
+let marks = "33ab"
+let paisa = null
+
+console.log(typeof score)
+console.log(typeof(score))
+console.log(typeof(marks))
+
+let valueInNumber = Number(marks)
+console.log(valueInNumber)
+
+// "33 " => 33
+// "33aba" => NaN
+console.log(typeof valueInNumber)
+console.log(typeof marks)
+console.log(paisa)
+console.log(typeof paisa)
+
+let isLogin = true
+console.log(typeof isLogin)
