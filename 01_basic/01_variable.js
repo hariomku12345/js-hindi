07d96b26de500c3d5a7275c570f364ef68cf26b8
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-29 01:53:12 +0000

    basic variable knowledge

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,5 +1,14 @@
 const accountId = 12345
 let accountEmail = "hariomkumar5190@gmail.com"
 var accountPassword = "0011"
+accountCity = "patna"
 
-console.table([accountId,accountEmail,accountPassword])
+let accountState
+
+// accountId = 123
+// we can't change the value of const
+accountEmail = "hariom@gmail.com"
+accountPassword = "001122"
+accountCity = "siwan"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
