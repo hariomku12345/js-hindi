5c4ae8daab92a8b59366424a9ba115f69b18791b
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-29 02:25:35 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer js code
+
+// alert("hello")  // we are using nodejs not browser
+
+let name = "Hariom"
+let age = 22
+let isLoggedIn = "yes"
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof "Hariom")
+console.log(typeof "yes")
+console.log(typeof null)
