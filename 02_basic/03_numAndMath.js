c8ab3528f19b12ad886062a357a54e7315e0cdb0
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-30 02:43:18 +0000

    number and math

---
+++ 02_basic/03_numAndMath.js
@@ -0,0 +1,8 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+// we can make number to string and apply all property of string on it
+console.log(balance.toString().length)
