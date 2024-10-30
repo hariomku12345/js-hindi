2e5bd443fb8eb8c2ea2719dc3bfde16f9c29daf7
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-10-30 13:29:18 +0000

    maths and number

--- 02_basic/03_numAndMath.js
+++ 02_basic/03_numAndMath.js
@@ -1,8 +1,23 @@
-const score = 400
-console.log(score)
+// const score = 400
+// console.log(score)
 
-const balance = new Number(100)
-console.log(balance)
+// const balance = new Number(100)
+// console.log(balance)
 
 // we can make number to string and apply all property of string on it
-console.log(balance.toString().length)
+// console.log(balance.toString().length)
+
+// point . ke baad kitna 0 hoga uske liye tofixed use karte h
+// console.log(balance.toFixed(3))
+
+// const otherNum = 23.1253
+
+// precision show totel kitna digit dikhana h
+// console.log(otherNum.toPrecision(4))
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString())
+// console.log(hundred.toLocaleString('en-IN'))
+
+// ++++++++++++++ Maths +++++++++++++++
+
