34acfe8a72a2f287807837b075101c8b5a234d83
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-03 12:18:27 +0000

    Date and time

---
+++ 02_basic/04_date.js
@@ -0,0 +1,22 @@
+// let d = new Date()
+// console.log(d)
+
+// let d = new Date()
+// console.log(d.toString())
+// console.log(d.toDateString())
+// console.log(d.toISOString())
+// console.log(d.toLocaleDateString())
+// console.log(typeof d)
+
+// let myDate = new Date(2020,11,3)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
