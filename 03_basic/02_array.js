ddc88e4d45bdc8cfe9836991575d1fac10c1215f
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-05 03:10:27 +0000

    array part2

--- 03_basic/02_array.js
+++ 03_basic/02_array.js
@@ -1,5 +1,36 @@
 const marval_hero = ["thor","ironman","spiderman"]
 const dc_hero = ["superman","flash","batman"]
 
-marval_hero.push(dc_hero)
-console.log(marval_hero)
+// marval_hero.push(dc_hero)
+// console.log(marval_hero)
+// console.log(marval_hero.length)
+// console.log(marval_hero[3])
+// console.log(marval_hero[3][2])
+
+// const allHero = marval_hero.concat(dc_hero)
+// console.log(allHero.length)
+// console.log(allHero)
+
+
+// spreade operator
+// it's work is to add two array, it is better than concate and push
+// const all_new_hero = [...marval_hero,...dc_hero]
+// console.log(all_new_hero)
+
+// const another_array = [1,2,3,[4,5],7,[8,9,[10,11]]]
+// console.log(another_array)
+// flat is use for same level array element karne ke liye
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+
+// console.log(Array.isArray("hariom"))
+// console.log(Array.from("hariom"))
+// console.log(Array.from({name : "hariom"})) // intresting case
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
