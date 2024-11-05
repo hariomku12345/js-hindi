cbf13bace11342cb8f324a33f057c42bbaf8f125
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-05 11:38:02 +0000

    Object part 2

--- 03_basic/04_object.js
+++ 03_basic/04_object.js
@@ -19,5 +19,34 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname)
-console.log(regularUser.fullname.userFullname.firstname);
+// console.log(regularUser.fullname)
+// console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1:"1",2:"2"}
+const obj2 = {3:"a",4:"2"}
+const obj4 = {5:"a",6:"2"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+// const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3)
+
+// database se jo value aata h o array of object ke form me aata h
+
+const users = [
+    {
+        id: 1,
+        email:"hari@gmsil.com"
+    },
+    {
+
+    }
+]
+console.log(users[0].email)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogin'))
