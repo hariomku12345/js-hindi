9185f479f8d309177c8acceb9c8def3349be212b
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-12 10:25:50 +0000

    conditional

---
+++ 05_control_flow/one.js
@@ -0,0 +1,19 @@
+// if
+//  if(condition){   // if constion true then {} ke ander wala code execute
+     // statement
+// }
+
+const isLoggedIn = true
+
+if(isLoggedIn){
+    console.log("yes")
+}
+
+
+// < ,>, <= , >=, == , != , === 
+
+// falsy value
+// false, 0 , -0 ', BigInt On , null , undefined , NaN 
+
+// TRUELY VALUE
+//  "0", "false ", " ", [], () , function(){}
