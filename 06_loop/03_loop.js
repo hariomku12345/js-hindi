35e5bc84d75019c9eb5114fa96754cafcc2c32af
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-15 01:55:47 +0000

    different loop

---
+++ 06_loop/03_loop.js
@@ -0,0 +1,39 @@
+// array specific loop
+
+// ["","",""]  array ke ander value
+// [{},{},{}]  array ke ander object h 
+
+// for of loop
+// ham for of loop ka use kahi bhi like array,string ,
+// number pe kar sakkte h
+
+const arr = [1,2,3,4,5]
+
+// for (const element of object) {
+    // element ke jagah koi variable aacha ss
+    // aur object ke jagah kis chij pe loop lagana h uska naam
+// }
+// for (const val of arr) {
+//     console.log(val); 
+// }
+
+// const greeting = 'hello world !'
+// for (const greet of greeting) {
+//     console.log(`each char is ${greet}`)
+// }
+
+// map -> key value pair . object
+
+const map = new Map()
+map.set('IN' , 'india')
+map.set('USA',"united state of america")
+map.set('Fr' , "france")
+
+// console.log(map)
+
+for (const key of map) {
+    console.log(key)
+}
+for (const [key,value] of map) {
+    console.log(key, ':-', value)
+}
