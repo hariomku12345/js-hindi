c3b8f0389fb40b0673fcfcbfd2c111bc48d777a9
Hariom kumar <140472733+hariomku12345@users.noreply.github.com>
2024-11-15 01:33:11 +0000

    while loop and do while loop

---
+++ 06_loop/02_loop.js
@@ -0,0 +1,30 @@
+// while loop
+
+// let index = 0
+// while(index <=10){
+//     console.log(`value of index is ${index}`)
+//     index = index+2
+// }
+
+
+// let myArray = ['hariom','sanjeet','krishna']
+// let arr = 0
+// while(arr < myArray.length)
+// {
+//     console.log(` value is ${myArray[arr]}`)
+//     arr++
+// }
+
+// do while loop
+// isme pahle kaam hoga fir condition check hoga
+//syntex
+//   do {
+
+//   }while(condition);
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)
